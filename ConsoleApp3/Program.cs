7eac7d12ed39678375737ed35a6424f8a847b54e
Kamil Kozik <kamilkozik14@gmail.com>
2022-11-19 16:40:30 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+
+        static bool czyPalindrom(string text)
+        {
+            
+            {
+                for (int i = 0; i < text.Length ; i++)
+                {
+                    {
+                        if (text[i] != text[text.Length - i - 1])
+
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine(czyPalindrom("kajak"));
+        }
+    }
+}

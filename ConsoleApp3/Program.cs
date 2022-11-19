795a19d57325a8489978c7546890bf38ac77d6d5
Kamil Kozik <kamilkozik14@gmail.com>
2022-11-19 16:58:11 +0100

    z osemkowego na dziesietny

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
 
-        static bool czyPalindrom(string text)
+       /* static bool czyPalindrom(string text)
         {
             if(text.Length==0) return false;
             {
@@ -18,10 +18,24 @@
                 return true;
             }
         }
+       */
+
+        static int from8to10(int number)
+        {
+            string numberString = number.ToString();
+            int wynik = 0;
+            for (int i = 0; i <numberString.Length; i++)
+            {
+                wynik +=(int) (int.Parse(numberString[i].ToString()) * Math.Pow(8, numberString.Length -i-1)); 
+            }
+        return wynik;
+        }
 
     static void Main(string[] args)
         {
-            Console.WriteLine(czyPalindrom("kajak"));
+            // Console.WriteLine(czyPalindrom("kajak"));
+            Console.WriteLine(from8to10(762));
+
         }
     }
 }
